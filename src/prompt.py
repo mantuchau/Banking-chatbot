56f07bcae66c741a812f25a4251e0c0efeed61f6
[Mantu Kumar] <212211006@nitdelhi.aac.in>
2025-01-24 00:32:41 +0530

    Pinecone vector database index created.

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt=(
+    "You are assistant for question answering task."
+    "Use follwing pieces of retrived context to answer."
+    "the question. you dont know the answer say,that you "
+    "Don't know use ,three sentence maximum and keep answer concise."
+    "\n\n"
+    "{context}"
+)
